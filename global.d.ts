985b9bc91916250354ab163460366ec42e56c85e
Oscar Tong <165961+m0o0scar@users.noreply.github.com>
2021-08-15 10:20:19 +0800

    chore: setup three-mesh-ui demo

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+declare module 'three-mesh-ui';
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,47 +2,8 @@ import * as THREE from 'three';
 import { Stage, VRButton } from '@nospoon/3utils';
 import { Record3DVideo } from './record3d/rgbd/Record3DVideo';
 
-import ThreeMeshUI from 'three-mesh-ui';
-import FontJSON from './assets/fonts/Roboto/Roboto-msdf.json';
-import FontImage from './assets/fonts/Roboto/Roboto-msdf.png';
-
 const DEFAULT_BACKGROUND_COLOR = 0x333333;
 const DEFAULT_CAMERA_POSITION = new THREE.Vector3(0, 0, 2);
-
-function makePanel(stage: Stage) {
-  const container = new ThreeMeshUI.Block({
-    justifyContent: 'center',
-    alignContent: 'center',
-    contentDirection: 'row-reverse',
-    fontFamily: FontJSON,
-    fontTexture: FontImage,
-    fontSize: 0.07,
-    padding: 0.02,
-    borderRadius: 0.11
-  });
-
-  container.position.set(0, 0.6, -0.3);
-  container.rotation.x = -0.55;
-  stage.scene.add(container);
-
-  const buttonOptions = {
-    width: 0.4,
-    height: 0.15,
-    justifyContent: 'center',
-    alignContent: 'center',
-    offset: 0.05,
-    margin: 0.02,
-    borderRadius: 0.075
-  };
-
-  const buttonNext = new ThreeMeshUI.Block(buttonOptions);
-  const buttonPrevious = new ThreeMeshUI.Block(buttonOptions);
-
-  buttonNext.add(new ThreeMeshUI.Text({ content: 'next' }));
-  buttonPrevious.add(new ThreeMeshUI.Text({ content: 'previous' }));
-
-  container.add(buttonNext, buttonPrevious);
-}
 
 async function main() {
   const supportVR = await VRButton.isVRSupported();
@@ -73,8 +34,6 @@ async function main() {
     stage.camera.position.set(x, y, z);
     stage.orbitControls.update();
   });
-
-  makePanel(stage);
 }
 
 main();
